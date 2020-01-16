e1104bbf97749f0e1fa050abbd4cc0517f257e8b
jkeown <jd.keown@yahoo.com>
2020-01-15 21:29:12 -0800

    UI redesign

---
+++ app.js
@@ -0,0 +1,17 @@
+function getNextFill(){
+	const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	const lastFillDate = new Date(document.querySelector('#lastFillDate').value);
+	
+	const daySupply = document. querySelector('#daySupply').value;
+	const dayOfMonth = lastFillDate.getDate();
+	lastFillDate.setDate(dayOfMonth + Number.parseInt(daySupply));
+	const nextFillDate = days[lastFillDate.getDay()] + " " + (lastFillDate.getMonth()+1) + "-" + lastFillDate.getDate() + "-" + lastFillDate.getFullYear();
+
+
+	document.querySelector('.date-due').innerHTML = 'Next fill is due on: <br/><br/>' + nextFillDate;
+};
+// let lastFillDate = new Date('8/8/2017');
+// let dayOfMonth = lastFillDate.getDate();
+// lastFillDate.setDate(dayOfMonth+61);
+
+// document.querySelector('.date-due').innerHTML = lastFillDate;
